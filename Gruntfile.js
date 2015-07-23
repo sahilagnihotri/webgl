81ed4db3822708a1928ff2605379fe9d3e43cc92
Sahil Agnihotri <sahilagnihotri@ymail.com>
2015-07-23 13:09:00 +0200

    update master branch, setting up framework for future useh

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-git');
+
+  grunt.initConfig({
+    jshint: {
+        all: ['code/*.js', 'Gruntfile.js']
+      },
+
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            'dist/{,*/}*',
+            '!dist/.git{,*/}*'
+          ]
+        }]
+      }
+    },
+
+    copy: {
+      dist: {
+        files: [
+          {src: ['index.html'], dest: 'dist/'},
+          {src: [
+              'common/**/*.js',
+              'code/**/*.js',
+              'code/**/*.html'
+            ],
+            dest: 'dist/',
+            expand: true
+          }
+        ]
+      }
+    },
+
+    'gh-pages': {
+      dist: {
+        options: {
+          base: 'dist',
+          message: 'Deployed by grunt gh-pages'
+        },
+        src: '**/*'
+      }
+    },
+
+  gitadd: {
+    task: {
+      options: {
+        force: true
+      },
+      files: {
+        src: ['**.*', '.gitignore', '	code/**.*', 'common/*', 'package.json']
+      }
+    }
+  }
+
+  });
+
+  grunt.registerTask('dist', ['clean:dist', 'copy:dist']);
+  grunt.registerTask('deploy', ['dist', 'gh-pages:dist']);
+
+};

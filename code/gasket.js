b8db95dd1374ee91b6cc024bd3ce06b7bd4e7771
Sahil Agnihotri <sahilagnihotri@ymail.com>
2015-07-23 16:39:18 +0200

    Deployed by grunt gh-pages

--- code/gasket.js
+++ code/gasket.js
@@ -2,21 +2,30 @@
 
 var canvas;
 var gl;
+var ctx; //debuging
 
 var points = [];
-var numTimesToSubdivide = 0;
-var angle = 30; // initila angle
+var numTimesToSubdivide = 4;
+var angle = 45; // initila angle
 var bufferId;
-var renderMode;
+var renderMode = "homework";
+var fillType = "mesh";
+var geometry = "triangle";
+
+window.onload = init;
+
 function init()
 {
     canvas = document.getElementById( "gl-canvas" );
-
     gl = WebGLUtils.setupWebGL( canvas );
+
+    ctx = WebGLDebugUtils.makeDebugContext(canvas.getContext("webgl"));
+    gl = WebGLDebugUtils.makeDebugContext(gl, logAndValidate);
+
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.clearColor( 0.0, 0.5, 0.5, 1.0 );
 
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
@@ -32,17 +41,47 @@ function init()
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    document.getElementById("slider").onchange = function(event) {
-    numTimesToSubdivide = parseInt(event.target.value);
-          renderTriangle();
+    document.getElementById("subDivideSlider").onchange = function(event) {
+      numTimesToSubdivide = parseInt(event.target.value);
+      console.log(event.target.value);
+      renderTriangle();
     };
 
     document.getElementById("twistValue").onchange = function(){
-    console.log(event.target.value);
-    angle = parseInt(event.target.value);
-    renderTriangle();
+      console.log(event.target.value);
+      angle = parseInt(event.target.value);
+      renderTriangle();
     };
+
+    document.getElementById("fill").onchange = function(){
+      console.log(event.target.value);
+      fillType = event.target.value;
+      renderTriangle();
+    };
+
+    render();
+};
+
+function gasket()
+{
+
+}
+
+function render(){
+  if ( geometry == "triangle") renderTriangle();
+  if ( geometry == "square") renderSquare();
+};
+
+function reset()
+{
+//    points = [];
+    document.getElementById('subDivideSlider').value = 4;
+    numTimesToSubdivide = 4;
+    document.getElementById("twistValue").value = 30;
+    angle = 30;
+    document.getElementById('demoType').selectedIndex = 1;
     renderTriangle();
+
 };
 
 var calculateDistance = function(vec2Point) {
@@ -80,7 +119,7 @@ function rotate( p, theta) {
 
 function divideTriangle( a, b, c, count )
 {
-    if ( count == 0 ) {
+    if ( count === 0 ) {
         triangle( a, b, c );
     }
     else {
@@ -121,7 +160,55 @@ function divideSquare( a, b, c, d, count )
     }
 }
 
-window.onload = init;
+var addSquare = function(a, b, c, e) {
+  points.push(a, b, c);
+  points.push(c, e, a);
+};
+
+function divideSquare( a, b, c, d, count )
+{
+    if ( count == 0 ) {
+        addSquare( a, b, c, d );
+    }
+    else {
+      var ad = mix(a, d, 0.5);
+      var ab = mix(a, b, 0.5);
+      var bc = mix(b, c, 0.5);
+      var cd = mix(c, e, 0.5);
+      var adbc = mix(ad, bc, 0.5);
+      --count;
+      divideSquare(ab, b, bc, adbc, count);   // bottom right
+      divideSquare(ad, adbc, cd, d, count);   // top left
+      divideSquare(adbc, bc, c, cd, count);   // top right
+      divideSquare(a, ab, adbc, ad, count);   // bottom left
+    }
+}
+
+////////// Debug fxns //////////
+function throwOnGLError(err, funcName, args) {
+  throw WebGLDebugUtils.glEnumToString(err) + " was caused by call to: " + funcName;
+};
+
+function logGLCall(functionName, args) {
+   console.log("gl." + functionName + "(" +
+      WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");
+}
+
+function validateNoneOfTheArgsAreUndefined(functionName, args) {
+  for (var ii = 0; ii < args.length; ++ii) {
+    if (args[ii] === undefined) {
+      console.error("undefined passed to gl." + functionName + "(" +
+                     WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");
+    }
+  }
+}
+
+function logAndValidate(functionName, args) {
+   logGLCall(functionName, args);
+   validateNoneOfTheArgsAreUndefined (functionName, args);
+}
+
+//////////////////////////////
 
 function renderTriangle()
 {
@@ -138,20 +225,69 @@ function renderTriangle()
     ];
 
     points = [];
-    divideTriangle( vertices[0], vertices[1], vertices[2],
-                    numTimesToSubdivide);
+    divideTriangle( vertices[0], vertices[1], vertices[2], numTimesToSubdivide);
 
   //  var radians = (Math.PI / 180) * 30;
     var rotatedPoints = points.map(function(vertex) {
       return rotate(vertex, angle);
     });
 
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
-    gl.clear( gl.COLOR_BUFFER_BIT );
-//    gl.drawArrays( gl.TRIANGLES, 0, points.length );
-  //  gl.drawArrays( gl.LINE_LOOP, 0, points.length );
+//   gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+  gl.clear( gl.COLOR_BUFFER_BIT );
+
+  if (fillType === 'solid') {
+    gl.drawArrays( gl.TRIANGLES, 0, points.length );
+  }
+
+  if (fillType === 'mesh') {
     for (var i=0; i<points.length; i+=3)
       gl.drawArrays( gl.LINE_LOOP, i, 3);
+  }
+  points = [];
+
+}
+
+function renderSquare()
+{
+  var vertices = [
+         vec2( 0,  1),
+         vec2( 1,  0),
+         vec2(-1,  0 ),
+         vec2( 0, -1)
+     ];
+
+/*     var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5, -0.5, -0.5, 1.0 )
+    ];
+*/
     points = [];
-    //requestAnimFrame(render);
+    divideSquare( vertices[0], vertices[1], vertices[2], vertices[3],
+                    numTimesToSubdivide);
+
+    var rotatedPoints = points.map(function(vertex) {
+      return rotate(vertex, angle);
+    });
+
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    if (fillType === 'solid') {
+      gl.drawArrays( gl.TRIANGLES, 0, points.length );
+    }
+
+    if (fillType === 'mesh') {
+      for (var i=0; i<points.length; i+=4)
+        gl.drawArrays( gl.LINE_LOOP, i, 4);
+    }
+    points = [];
 }

7a3bfa8ac18999f8284bbaea61df0603bdeb399c
Sahil Agnihotri <sahilagnihotri@ymail.com>
2015-07-26 00:43:11 +0200

    Deployed by grunt gh-pages

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ It is practice/experiment repository.
 Using grunt for publishing, copying, cleaning, etc.
 Issues on [issues pages](https://github.com/sahilagnihotri/webgl/issues)
 
+For debugging purpose you can use server.js, its small http-server using node.js
+
+
 [sahilagnihotri](http://sahilagnihotri.github.io/)
---
+++ code/color.js
@@ -0,0 +1,55 @@
+
+var gl;
+var points;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    var vertices = [-1, -1, 0, 1, 1, -0.5];
+    var colors = [ 1,0,0, 0,1,0, 0,0,1];
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+/*    var cbufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cbufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation( program, "vColor");
+    gl.vertexAttribPointer( vColor, 3, gl.FLOAT, false, 0,0);
+    gl.enableVertexAttribArray(vColor);
+*/
+    var colorLoc = gl.getUniformLocation( program, "color" );
+    gl.uniform4f( colorLoc, 1.0, 0.0, 0.0, 1.0 );
+
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+}
